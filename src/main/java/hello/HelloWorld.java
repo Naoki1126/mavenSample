d48e5da4ce94c286031697b650c33cb88cae53fd
NaokiMaruyama <m.naoki85@gmail.com>
2023-09-18 17:40:45 +0900

    firstCommit

---
+++ src/main/java/hello/HelloWorld.java
@@ -0,0 +1,12 @@
+package hello;
+
+import java.time.LocalTime;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		LocalTime currentTime = LocalTime.now();
+		System.out.println("The cutrrent local time is: " + currentTime);
+		Greeter greeter = new Greeter();
+		System.out.println(greeter.sayHello());
+	}
+}
